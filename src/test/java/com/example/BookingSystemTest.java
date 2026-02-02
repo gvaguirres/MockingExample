79aa2e4a1748eeeabfc6d168095366396a3138e7
Gabriela Aguirre <gvaguirres@gmail.com>
2026-02-02 11:11:21 +0100

    Add tests to check method getAvailableRooms in BookingSystem class

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -1,6 +1,5 @@
 package com.example;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,6 +11,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -34,14 +34,14 @@ public class BookingSystemTest {
     BookingSystem bookingSystem;
 
     @ParameterizedTest
-    @MethodSource("provideNullScenarios")
+    @MethodSource("provideNullScenariosForRoomIdStartAndEndTime")
     void throwsExceptionWhenRoomIdStartTimeOrEndTimeIsNull(String roomId, LocalDateTime startTime, LocalDateTime endTime) {
 
         var exception = assertThrows(IllegalArgumentException.class,
                 () -> bookingSystem.bookRoom(roomId, startTime, endTime));
         assertThat(exception).hasMessage("Bokning kräver giltiga start- och sluttider samt rum-id");
     }
-    static Stream<Arguments> provideNullScenarios() {
+    private static Stream<Arguments> provideNullScenariosForRoomIdStartAndEndTime() {
         LocalDateTime timeNow = LocalDateTime.now();
         return Stream.of(
                 Arguments.of(null, timeNow, timeNow),
@@ -98,5 +98,99 @@ public class BookingSystemTest {
         assertThat(exception).hasMessage("Sluttid måste vara efter starttid");
 
     }
+
+    @Test
+    void bookRoomIfRoomIsAvailable() {
+
+        Room room = new Room("100", "Room1");
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime startTime = currentTime.plusDays(2);
+        LocalDateTime endTime = currentTime.plusDays(3);
+
+        Mockito.when(timeProvider.getCurrentTime()).thenReturn(currentTime);
+        Mockito.when(roomRepository.findById(room.getId())).thenReturn(Optional.of(room));
+
+        boolean result = bookingSystem.bookRoom(room.getId(), startTime, endTime);
+
+        assertThat(result).isTrue();
+
+        Mockito.verify(roomRepository).save(room);
+    }
+
+    @Test
+    void notBookRoomIfRoomIsNotAvailable() {
+
+        Room room = new Room("100", "Room1");
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime startTime = currentTime.plusDays(1);
+        LocalDateTime endTime = currentTime.plusDays(2);
+        Booking booking = new Booking("1", "100", startTime, endTime);
+
+        Mockito.when(timeProvider.getCurrentTime()).thenReturn(currentTime);
+        Mockito.when(roomRepository.findById(room.getId())).thenReturn(Optional.of(room));
+        room.addBooking(booking);
+
+        boolean result = bookingSystem.bookRoom(room.getId(), startTime, endTime);
+
+        assertThat(result).isFalse();
+
+        Mockito.verify(roomRepository, Mockito.never()).save(room);
+
+    }
+
+    //Test missing
+    //To check if the notification is sent when a room is booked
+
+    @ParameterizedTest
+    @MethodSource("provideNullScenarios")
+    void getAvailableRoomsThrowsExceptionWhenStartOrEndTimeIsNull(LocalDateTime startTime, LocalDateTime endTime) {
+
+        var exception = assertThrows(IllegalArgumentException.class,
+                () -> bookingSystem.getAvailableRooms(startTime, endTime));
+        assertThat(exception).hasMessage("Måste ange både start- och sluttid");
+    }
+    private static Stream<Arguments> provideNullScenarios() {
+        LocalDateTime timeNow = LocalDateTime.now();
+        return Stream.of(
+                Arguments.of(timeNow, null),
+                Arguments.of(null, timeNow),
+                Arguments.of(null, null)
+        );
+    }
+
+    @Test
+    void getAvailableRoomsThrowsExceptionWhenEndTimeIsBeforeStartTime() {
+
+            LocalDateTime currentTime = LocalDateTime.now();
+            LocalDateTime startTime = currentTime.plusDays(4);
+            LocalDateTime endTime = currentTime.plusDays(3);
+
+            var exception = assertThrows(IllegalArgumentException.class,
+                    () -> bookingSystem.getAvailableRooms(startTime, endTime));
+            assertThat(exception).hasMessage("Sluttid måste vara efter starttid");
+    }
+
+    @Test
+    void getAvailableRoomsReturnsAListOfAvailableRooms() {
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime startTime = currentTime.plusDays(4);
+        LocalDateTime endTime = currentTime.plusDays(5);
+        Room room1 = new Room("101", "Room1");
+        Room room2 = new Room("102", "Room2");
+
+        Mockito.when(roomRepository.findAll()).thenReturn(List.of(room1, room1));
+        List<Room> result = bookingSystem.getAvailableRooms(startTime, endTime);
+
+        assertThat(result).hasSize(2);
+
+    }
+
+    @Test
+    void cancelBookingThrowsExceptionWhenBookingIdIsNull() {
+
+    }
+
+
 
 }
